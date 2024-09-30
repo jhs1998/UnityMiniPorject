741c2f02f04baecd0dd3d9b0d016566a94a17f78
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-09-30 18:47:39 +0900

    [feat] 드로우 배치 완료
    게임 시작시 1초씩 5드로우.
    정해진 핸드에 카드가 배치.

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -1,18 +1,41 @@
-using System.Collections;
+using DG.Tweening;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class CardManager : MonoBehaviour
 {
-    public static CardManager Inst {  get; private set; }
-    void Awake () => Inst = this;
+    public static CardManager Inst { get; private set; }
+    void Awake() => Inst = this;
 
     [SerializeField] CardSkillSO cardskillSO;
-    [SerializeField] GameObject cardPrefab;
+
+    [SerializeField] GameObject attackCard01;  // 약공격 카드 프리팹
+    [SerializeField] GameObject attackCard02;  // 골부수기 카드 프리팹
+    [SerializeField] GameObject guardCard;     // 막기 카드 프리팹
+    [SerializeField] GameObject healingCard;   // 치유 카드 프리팹
+    [SerializeField] Transform cardParent;
+    // 카드 생성 위치
+    [SerializeField] Transform cardspon;
+
+    // 카드 배치 범위
+    [SerializeField] Vector2 minSpawnPosition;
+    [SerializeField] Vector2 maxSpawnPosition;
+
+  
 
     List<Skill> SkillBuffer;
+    private List<GameObject> activeCards = new List<GameObject>(); // 활성 카드 리스트
 
+    public Skill PopCard()
+    {
+        if (SkillBuffer.Count == 0)
+            SetupSkillBuffer();
+
+        Skill skill = SkillBuffer[0];
+        SkillBuffer.RemoveAt(0);
+        return skill;
+    }
 
     void SetupSkillBuffer()
     {
@@ -32,13 +55,94 @@ public class CardManager : MonoBehaviour
             SkillBuffer[rand] = temp;
         }
     }
+
     private void Start()
-    {
+    {       
+
         SetupSkillBuffer();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Invoke("AddCard", i);
+        }
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+            AddCard();
+    }
+
 
     void AddCard()
     {
+        Skill skill = PopCard();
+        GameObject cardPrefab = FindName(skill.name);
 
+        if (cardPrefab != null)
+        {
+            Vector3 spawnPosition = cardspon.transform.position;
+            Quaternion spawnRotation = cardspon.transform.rotation;
+
+            GameObject newCard = Instantiate(cardPrefab, spawnPosition, spawnRotation, cardParent);
+            newCard.name = skill.name; // 생성된 카드 오브젝트의 이름을 설정
+
+            // 카드 리스트에 추가
+            activeCards.Add(newCard);
+
+            // 카드 정렬 및 이동
+            AlignCards();
+
+            // 카드 핸드에 이동
+            MoveCardToHand(newCard);
+        }
+        else
+        {
+            Debug.LogError($"프리팹이 없습니다: {skill.name}");
+        }
+    }
+
+    GameObject FindName(string cardName)
+    {
+        switch (cardName)
+        {
+            case "약공격":
+                return attackCard01;
+            case "골부수기":
+                return attackCard02;
+            case "막기":
+                return guardCard;
+            case "치유":
+                return healingCard;
+            default:
+                return null; // 해당하는 프리팹이 없으면 null 반환
+        }
+    }
+    void MoveCardToHand(GameObject card)
+    {
+        AlignCards(); // 카드 정렬 호출
+    }
+
+    void AlignCards()
+    {
+        int cardCount = activeCards.Count;
+        float spacing = Mathf.Max(50f, 200f / cardCount); // 카드 간격 조정
+
+        // 카드 배치 범위 계산
+        float startX = minSpawnPosition.x + (spacing / 2);
+        float endX = maxSpawnPosition.x - (spacing / 2);
+        float totalWidth = endX - startX;
+
+        // 중앙 정렬을 위한 오프셋
+        float offset = Mathf.Max(0, totalWidth - (spacing * cardCount)) / 2;
+
+        for (int i = 0; i < cardCount; i++)
+        {
+            GameObject card = activeCards[i];
+            Vector3 targetPosition = new Vector3(startX + offset + (i * spacing), minSpawnPosition.y, 0);
+
+            // DOTween으로 카드 이동
+            card.transform.DOMove(targetPosition, 1f).SetEase(Ease.OutBounce);
+        }
     }
 }
--- Assets/Scripts/Cardcontroller.cs
+++ Assets/Scripts/Cardcontroller.cs
@@ -4,16 +4,34 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
 public class Cardcontroller : MonoBehaviour, IDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public PRS originPRS;
     private Vector3 originalScale;
+
     private void Start()
     {
         // 처음 크기 저장
         originalScale = transform.localScale;
     }
-
+    public void MoveTransform(PRS prs, bool useDotween, float dotweenTime = 0)
+    {
+        if (useDotween)
+        {
+            transform.DOMove(prs.pos, dotweenTime);
+            transform.DORotateQuaternion(prs.rot, dotweenTime);
+            transform.DOScale(prs.scale, dotweenTime);
+        }
+        else
+        {
+            transform.position = prs.pos;
+            transform.rotation = prs.rot;
+            transform.localScale = prs.scale;
+        }
+    }
     public void OnDrag(PointerEventData eventData)
     {
         transform.Translate(eventData.delta);
