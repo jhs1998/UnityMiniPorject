aa90c4234b634a27e36ec2500e9ea2d593bc15ff
jhs1998 <105158325+jhs1998@users.noreply.github.com>
2024-10-02 11:54:31 +0900

    [feat] UI 체력바 업데이트
    체력바가 체력 수치에 맞게 줄어들게 작업

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
+using UnityEngine.Animations.Rigging;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,14 +11,14 @@ public class GameManager : MonoBehaviour
     //전투 상황
     public enum State
     {
-        start, playerTurn, enemyTurn, win
+        start, playerTurn, enemyTurn, win, lose
     }
 
     // 플레이어 오브젝트
-
+    [SerializeField] GameObject player;
     // 적 오브젝트 1, 2
-
-    // 턴넘김 버튼
+    [SerializeField] GameObject enemy1;
+    [SerializeField] GameObject enemy2;
 
     // 나중에 현재 몇턴째인지도 추가 가능
 
@@ -24,56 +26,89 @@ public class GameManager : MonoBehaviour
 
     //플레이어 체력 ui 3개
     // 체력 텍스트 1,2
+    public TextMeshProUGUI playerhptext1;
+    public TextMeshProUGUI playerhptext2;
     // 체력바
-
+    public Slider playerHpBarSlider;
     // 방어도 UI
 
     //적 체력 ui 1,2 
     // 체력바 1,2
+    public Slider monsterHpBarSlider01;
+    public Slider monsterHpBarSlider02;
     // 텍스트 1,2
+    public TextMeshProUGUI monsterhptext1;
+    public TextMeshProUGUI monsterhptext2;
 
     // 덱 숫자 UI
     [SerializeField] GameObject deckNum;
     private TextMeshProUGUI deckNumText;
     public static GameManager Inst { get; private set; }
+
+    // 턴 종료 버튼
+    [SerializeField] Button turnButton;
     // 코스트 숫자
 
-    //-----------
+    // 플레이어 턴 UI
+    [SerializeField] GameObject playerturnUI;
+    //--------------------------------
 
     // 플레이어 체력
+    [SerializeField] public float playerHP;
+
     // 적들 체력
+    [SerializeField] public float enemy01HP;
+    [SerializeField] public float enemy02HP;
 
     // 방어도
     // 전체 코스트
 
-    // 카드 숫자
-
-
-
     public State state;
+    public bool playerLive;
     public bool isLive1; // 1적 생존 여부
     public bool isLive2; // 2적 생존 여부
+    float playernowHP;
+    float enemynowHP01;
+    float enemynowHP02;
 
     private void Awake()
     {
         state = State.start; // 전투 시작 상태
+        playernowHP = playerHP;
+        enemynowHP01 = enemy01HP;
+        enemynowHP02 = enemy02HP;
+        playerLive = true;
+        isLive1 = true;
+        isLive2 = true;
+
+        UIupdate();
+
 
         Inst = this;
         deckNumText = deckNum.GetComponent<TextMeshProUGUI>();
+
+        turnButton.onClick.AddListener(ButtonClick);
+    }
+
+    private void Update()
+    {
+        if (state == State.start)
+        {
+            BattleStart();
+        }
+
     }
     
-
     void BattleStart()
     {
-        
+        Debug.Log("전투 시작");
         // 턴넘기기
         state = State.playerTurn;
+        PlayerAttackButton();
     }
 
     public void PlayerAttackButton()
     {
-        // 카드 사용으로 플레이어 행동
-
         if(state != State.playerTurn)
         {
             return;
@@ -84,17 +119,46 @@ public class GameManager : MonoBehaviour
     IEnumerator PlayerAttack()
     {
         yield return new WaitForSeconds(1f);
+
+        if (playerturnUI == true)
+            playerturnUI.SetActive(false);
+
+        // 카드를 사용해 플레이어가 공격
         Debug.Log("플레이어 공격");
-        if(!isLive1)
+        enemynowHP01 -= 10;
+        enemynowHP02 -= 10;
+        UIupdate();
+
+        if (enemynowHP01 <= 0)
+        {
+            enemynowHP01 = 0;
+            isLive1 = false;
+        }
+        else if(enemynowHP02 <= 0)
+        {
+            enemynowHP02 = 0;
+            isLive2 = false;
+        }
+
+        if (!isLive1 && !isLive2)
         {
             state = State.win;
             EndBattle();
         }
         else
         {
+            // 대기중
+            Debug.Log("플레이어 공격 완료. 턴을 넘기세요.");
+        }
+    }
+    public void ButtonClick()
+    {
+        if (state == State.playerTurn)
+        {
+            Debug.Log("턴 넘김");
             state = State.enemyTurn;
             StartCoroutine(EnemyTurn());
-        }
+        }        
     }
 
     void EndBattle()
@@ -104,17 +168,61 @@ public class GameManager : MonoBehaviour
 
     IEnumerator EnemyTurn()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(2f);
         // 적이 공격
-
+        Debug.Log("적의 공격");
+        playernowHP -= 5;
         // 적 공격이 끝나면 플레이어에게 턴 넘김
+        UIupdate();
 
-        state = State.playerTurn;
-        
+        if (playernowHP <= 0)
+        {
+            Debug.Log("플레이어 사망");
+            playernowHP = 0;
+            playerLive = false;
+        }
+
+        if (!isLive1 && !isLive2)
+        {
+            state = State.win;
+            EndBattle();
+        }
+        else if (!playerLive)
+        {
+            Debug.Log("플레이어 사망");
+            state = State.lose;
+            EndBattle();
+        }
+        else
+        {
+            state = State.playerTurn;
+            Debug.Log("적의 공격 완료. 플레이어의 턴입니다.");
+            playerturnUI.SetActive(true);
+            yield return new WaitForSeconds(1f);
+            PlayerAttackButton();
+        }        
     }
+
     // 덱 숫자 적용
     public void UpdateDeckNum(int num)
     {
         deckNumText.text = num.ToString();
+    }
+
+    // 체력 텍스트 업데이트
+    private void UIupdate()
+    {
+        playerhptext1.text = ($"{playernowHP}/{playerHP}");
+        playerhptext2.text = ($"{playernowHP}/{playerHP}");
+        monsterhptext1.text = ($"{enemynowHP01}/{enemy01HP}");
+        monsterhptext2.text = ($"{enemynowHP02}/{enemy02HP}");
+        HPScrollbar();
+    }
+    // 체력바 업데이트
+    public void HPScrollbar()
+    {
+        playerHpBarSlider.value = playernowHP / playerHP;
+        monsterHpBarSlider01.value = enemynowHP01 / enemy01HP;
+        monsterHpBarSlider02.value = enemynowHP02 / enemy02HP;
     }
 }
